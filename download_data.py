8200aa9e22fcb034f92ea0e1f0180bd00f9b8f20
Emil Martens <emil.martens@gmail.com>
2021-01-27 21:35:31 +0100

    all data in one table

--- download_data.py
+++ download_data.py
@@ -3,6 +3,8 @@ import pandas as pd
 from multiprocessing.pool import ThreadPool
 from itertools import product as iterprod
 import os
+import numpy as np
+import re
 
 url_root = "https://hjorteviltregisteret.no/Statistikk"
 
@@ -11,7 +13,7 @@ animals = [
     'Hjort']
 
 
-data_set = [
+data_sets = [
     'Sett',
     'Felt',
     'Slaktevekt',
@@ -42,10 +44,48 @@ if not os.path.exists('data'):
     os.makedirs('data')
 
 with ThreadPool(128) as pool:
-    pool.map(req, iterprod(animals, data_set))
+    pool.map(req, iterprod(animals, data_sets))
 
 
-data_tables = []
-for animal, data_set in iterprod(animals, data_set):
+frames = []
+for animal, data_set in list(iterprod(animals, data_sets)):
     filename = f'data/{animal}_{data_set}.xls'
-    data_tables.append(pd.read_excel(filename))
+    frame = pd.read_excel(filename)
+    frame = frame.add_prefix(f'{animal} {data_set} ')
+
+    columns = list(frame.columns)
+
+    double_names = any(['unnamed' in i.lower() for i in columns])
+    if double_names:
+        columns = [columns[i] if 'unnamed' not in columns[i].lower()
+                   else columns[i-1]
+                   for i in range(len(columns))]
+
+        columns = [i + ' ' + str(j) if not pd.isnull(j)
+                   else i
+                   for i, j in zip(columns, frame.values[0])]
+
+        frame.columns = columns
+        frame = frame.iloc[1:]
+    frames.append(frame)
+
+dataframe = pd.concat([frames[0].iloc[:, :3]]+[i.iloc[:, 3:] for i in frames],
+                      axis=1)
+dataframe.rename(columns={dataframe.columns[0]: "jaktår",
+                          dataframe.columns[0]: "kommunenr",
+                          dataframe.columns[0]: "kommune"}, inplace=True)
+columns = list(dataframe.columns)
+for i, text in enumerate(columns):
+    text = text.lower()
+    text = re.sub(' ½', '.5', text)
+    text = re.sub(r'([0-9]*\.[0-9]*) år og eldre', r'>=\1 år', text)
+    text = re.sub('gjennomsnittsvekt', 'snittvekt', text)
+    text = re.sub('kalv', ' 0.5 år', text)
+    columns[i] = text
+
+
+dataframe.columns = columns
+
+dataframe.iloc[:, :2] = dataframe.iloc[:, :2].astype(np.int32)
+'Våler', 3018
+'Herøy', 1515
