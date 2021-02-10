47a09fc4c819c3008f8e125ee59414acffc24262
Emil Martens <emil.martens@gmail.com>
2021-02-10 12:07:58 +0100

    cleaned up get hjortevillt data

--- src/hjortevilt.py
+++ src/hjortevilt.py
@@ -10,85 +10,80 @@ SAVE_EXCEL = False
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 DATA_FOLDER_PATH = os.path.join(dir_path, '..', 'data')
-DATAFRAME_FILENAME = os.path.join(DATA_FOLDER_PATH,
-                                  'hjorteviltregisteret.csv')
 
-url_root = "https://hjorteviltregisteret.no/Statistikk"
-
-animals = [
-    'Elg',
-    'Hjort']
+RAW_FILENAME = os.path.join(DATA_FOLDER_PATH,
+                            'hjorteviltregisteret_raw.csv')
+HJORTEDATA_FILENAME = os.path.join(DATA_FOLDER_PATH,
+                                   'hjortedata.csv')
 
 
-data_sets = [
-    'Sett',
-    'Felt',
-    'Slaktevekt',
-]
-
-form = {
-    'fraår': 1985,
-    'granularitet': 1,
-    'gruppering': 2,
-    'tilår': 2020,
-}
-
-
-def get_frames(input):
+def download_from_hjortevilltregisteret(input):
     """Used to download the data and translate it into a dataframe"""
+
+    url_root = "https://hjorteviltregisteret.no/Statistikk"
     animal, data_set = input
+    form = {
+        'fraår': 1985,
+        'granularitet': 1,
+        'gruppering': 2,
+        'tilår': 2020,
+    }
 
     url = '/'.join((url_root, animal, 'Jaktmateriale', data_set)) + 'Excel'
     rep = requests.get(url, params=form)
 
-    frame = pd.read_excel(rep.content)
-    frame = frame.add_prefix(f'{animal} {data_set} ')
+    df = pd.read_excel(rep.content)
+    df = df.add_prefix(f'{animal} {data_set} ')
 
     # In some of the tables, the first and second row are labels, this solve it
-    if any(['unnamed' in i.lower() for i in frame.columns]):  # second row is also name
-        columns = list(frame.columns)
+    if any(['unnamed' in i.lower() for i in df.columns]):  # second row is also name
+        columns = list(df.columns)
         columns = [columns[i] if 'unnamed' not in columns[i].lower()
                    else columns[i-1]
                    for i in range(len(columns))]
 
         columns = [i + ' ' + str(j) if not pd.isnull(j)
                    else i
-                   for i, j in zip(columns, frame.values[0])]
+                   for i, j in zip(columns, df.values[0])]
 
-        frame.columns = columns
-        frame = frame.iloc[1:]
-    return frame
+        df.columns = columns
+        df = df.iloc[1:]
+    return df
 
 
-def get_hjortevilt_dataframe(reload=False, redownload=False):
+def get_raw_hjortevillt_df():
     """Get the data from hjorteviltregisteret
-    reload: used for debugging
-    redownload: used for debugging
     """
+    animals = [
+        'Elg',
+        'Hjort']
 
-    # if the file is already created, just load it
-    if os.path.isfile(DATAFRAME_FILENAME) and not redownload:
-        dataframe = pd.read_csv(DATAFRAME_FILENAME)
-        if not reload:
-            return dataframe
+    data_sets = [
+        'Sett',
+        'Felt',
+        'Slaktevekt',
+    ]
+    # download the data and turn each table into an individual frame
+    with ThreadPool(6) as pool:
+        frames = pool.map(download_from_hjortevilltregisteret,
+                          iterprod(animals, data_sets))
+    # join the frames into one single large frame
+    df = pd.concat([frames[0].iloc[:, :3]]
+                   + [i.iloc[:, 3:] for i in frames],
+                   axis=1)
+    df.to_csv(RAW_FILENAME, index=False)
+    return df
 
-    else:
-        # download the data and tur each table into an individual frame
-        with ThreadPool(6) as pool:
-            frames = pool.map(get_frames, iterprod(animals, data_sets))
-        # join the frames into one single large frame
-        dataframe = pd.concat([frames[0].iloc[:, :3]]
-                              + [i.iloc[:, 3:] for i in frames],
-                              axis=1)
 
-    dataframe.rename(columns={dataframe.columns[0]: "jaktår",
-                              dataframe.columns[1]: "kommunenr",
-                              dataframe.columns[2]: "kommune"}, inplace=True)
+def preprocess(df):
+    df.rename(columns={df.columns[0]: "jaktår",
+                       df.columns[1]: "kommunenr",
+                       df.columns[2]: "kommune"}, inplace=True)
 
     # this only removes the last row, which is garbage
-    dataframe.dropna(subset=dataframe.columns[:3], inplace=True)
+    df.dropna(subset=df.columns[:3], inplace=True)
 
-    columns = list(dataframe.columns)
+    columns = list(df.columns)
     for i, text in enumerate(columns):
         text = text.lower()
         text = re.sub(' ½', '.5', text)
@@ -97,12 +92,46 @@ def get_hjortevilt_dataframe(reload=False, redownload=False):
         # text = re.sub(' ?kalv(er)?', ' 0.5 år', text)
         columns[i] = text
 
-    dataframe.columns = columns
-    dataframe.iloc[:, :2] = dataframe.iloc[:, :2].astype(np.int32)
+    df.columns = columns
+    df.iloc[:, :2] = df.iloc[:, :2].astype(np.int32)
 
-    # there were some error in the data??? våler and herøy had multiple nr
-    dataframe.loc[dataframe.loc[:, 'kommune'] == 'Våler', 'kommunenr'] = 3018
-    dataframe.loc[dataframe.loc[:, 'kommune'] == 'Herøy', 'kommunenr'] = 1515
+    # multiple municipals sharing name
+    df.loc[(df['kommune'] == 'Våler') & (df['kommunenr'] == 3018),
+           'kommune'] = 'Våler (Østfold)'
+    df.loc[(df['kommune'] == 'Våler') & (df['kommunenr'] == 3419),
+           'kommune'] = 'Våler (Hedmark)'
+    df.loc[(df['kommune'] == 'Herøy') & (df['kommunenr'] == 1818),
+           'kommune'] = 'Herøy (Nordland)'
+    df.loc[(df['kommune'] == 'Herøy') & (df['kommunenr'] == 1515),
+           'kommune'] = 'Herøy (Møre og Romsdal)'
 
-    dataframe.to_csv(DATAFRAME_FILENAME, index=False)
-    return dataframe
+    df.set_index(['kommunenr', 'jaktår'], inplace=True)
+    df.sort_index(0, ['kommunenr', 'jaktår'])
+
+    return df
+
+
+def get_hjortevillt_data(reprocess=False, redownload=False):
+
+    if os.path.isfile(HJORTEDATA_FILENAME) and not reprocess:
+        df = pd.read_csv(HJORTEDATA_FILENAME)
+        return df
+
+    if os.path.isfile(RAW_FILENAME) and not redownload:
+        df_raw = pd.read_csv(RAW_FILENAME)
+    else:
+        df_raw = get_raw_hjortevillt_df()
+        df_raw.to_csv(RAW_FILENAME, index=False)
+
+    df = preprocess(df_raw)
+
+    kommune_nr_name_relations = dict(
+        df.reset_index().groupby(['kommunenr', 'kommune']).
+        size().reset_index().iloc[:, :-1].values)
+
+    df.drop('kommune', axis=1, inplace=True)
+    extra_data = {'kommune_names': kommune_nr_name_relations}
+    return df, extra_data
+
+
+df = get_hjortevillt_data()
