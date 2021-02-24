a1a06bedfb4ab9646763e0e54e749ca54fa5f28e
Emil Laptop <emil.martens@gmail.com>
2021-02-24 15:23:26 +0100

    small stuff

--- src/plsr.py
+++ src/plsr.py
@@ -1,16 +1,23 @@
+from pandas.core.frame import DataFrame
 from sklearn.cross_decomposition import PLSRegression
 from hjortevilt import get_hjortevillt_data
-
+import pandas as pd
 
 df, _ = get_hjortevillt_data()
 
+index_frame = df.index.to_frame()
+index_frame['neste jaktår'] = index_frame['jaktår'] + 1
+
+
+Y_index = pd.MultiIndex.from_frame(index_frame[['kommunenr', 'neste jaktår']])
+# index_frame[df.index.names] = df.reset_index()[df.index.names]
 diff = df.groupby('kommunenr').diff(-1)
 
-filt = df.notna().all(axis=1) & diff.notna().all(axis=1)
+# filt = df.notna().all(axis=1) & diff.notna().all(axis=1)
 
 
-X = df.loc[filt].values
-Y = diff.loc[filt].values
+# X = df.loc[filt].values
+# Y = diff.loc[filt].values
 
-pls2 = PLSRegression()
-pls2.fit(X, Y)
+# pls2 = PLSRegression()
+# pls2.fit(X, Y)
