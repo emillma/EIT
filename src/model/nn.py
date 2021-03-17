96f4cfc4085791f65bb4bc4ce5c3f282015cc8f2
Andreas Hammer Håversen <andreas.h.haaversen@gmail.com>
2021-03-17 10:35:45 +0100

    Added PLSR model

--- src/model/nn.py
+++ src/model/nn.py
@@ -1,11 +1,11 @@
 
-from __future__ import print_function
 from typing import List
 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Input, Dense, Dropout
+from tensorflow.keras.layers import Input, Dense, Dropout, BatchNormalization
 from tensorflow.keras.callbacks import EarlyStopping, TerminateOnNaN
 from tensorflow.keras.losses import mean_squared_error
+from sklearn.cross_decomposition import PLSRegression
 import pandas as pd
 
 from model.model import LogisticModel
@@ -16,6 +16,7 @@ class MGNN:
     def __init__(self, config: dict) -> None:
         layers = config["layers"]
         self.l3_model = Sequential()
+        self.l3_model.add(BatchNormalization())
         for i in range(len(layers)):
             if i == 0:
                 self.l3_model.add(Input(layers[i]['nodes']))
@@ -25,18 +26,25 @@ class MGNN:
             self.l3_model.add(Dropout(config["dropout_frac"]))
         self.l3_model.add(Dense(1, activation='linear'))
 
+        self.l2_model = PLSRegression(n_components=1)
+
         self.l1_model = LogisticModel(config["num_weather_params"])
 
     def train(self, config: dict, train_X: pd.DataFrame, train_Y: pd.DataFrame, last_year_key: str, weather_keys: List[str]):
 
-        self.l1_model.fit(train_X, train_Y)
+        self.l1_model.fit(train_X, train_Y, last_year_key, weather_keys)
 
-        model_prediction = self.l1_model.predict(
+        l1_prediction = self.l1_model.predict(
             train_X[last_year_key], train_X[weather_keys])
-        
-        model_error = train_Y - model_prediction
+        l1_error = train_Y - l1_prediction
+        train_X["l1_prediction"] = l1_prediction
 
-        train_X["model_prediction"] = model_prediction
+        self.l2_model.fit(train_X, l1_error)
+
+        l2_prediction = self.l2_model.predict(train_X)
+        train_X["l2_prediction"] = l2_prediction
+
+        remaining_error = l1_error - l2_prediction
 
         # Hyper-parameters of the training process
         num_epochs = config["epochs"]
@@ -52,27 +60,33 @@ class MGNN:
             monitor='val_loss_1', patience=patience_val, verbose=1)
 
         print(f'Running... {config["optimizer"]}')
-        self.l3_model.fit(train_X, model_error,
+        self.l3_model.fit(train_X, remaining_error,
                           batch_size=batch_size,
                           epochs=num_epochs,
                           verbose=1,
                           validation_split=val_frac, callbacks=[early_stopping, TerminateOnNaN()])
 
-    def save_models(self, model_name):
-        self.new_method(model_name)
-
     def predict(self, inputs: pd.DataFrame, last_year_key: str, weather_keys: List[str]):
-        model_prediction = self.l1_model.predict(
+        l1_prediction = self.l1_model.predict(
             inputs[last_year_key], inputs[weather_keys])
-        inputs["model_prediction"] = model_prediction
+        inputs["l1_prediction"] = l1_prediction
+
+        l2_prediction = self.l2_model.predict(inputs)
+        inputs["l2_prediction"] = l2_prediction
+
         nn_prediction = self.l3_model.predict(inputs)
 
-        return model_prediction + nn_prediction
+        return l1_prediction + l2_prediction + nn_prediction
 
-    def test(self, testX: pd.DataFrame, testY: pd.DataFrame, last_year_key: str, weather_keys: List[str]):
-        model_prediction = self.l1_model.predict(
-            testX[last_year_key], testX[weather_keys])
-        testX["model_prediction"] = model_prediction
-        model_error = model_prediction - testY
-        test_score = self.l3_model.evaluate(testX, model_error)
+    def test(self, inputs: pd.DataFrame, targets: pd.DataFrame, last_year_key: str, weather_keys: List[str]):
+        l1_prediction = self.l1_model.predict(
+            inputs[last_year_key], inputs[weather_keys])
+        inputs["l1_prediction"] = l1_prediction
+
+        l2_prediction = self.l2_model.predict(inputs)
+        inputs["l2_prediction"] = l2_prediction
+
+        remaining_error = targets - l1_prediction - l2_prediction
+
+        test_score = self.l3_model.evaluate(inputs, remaining_error)
         return test_score
