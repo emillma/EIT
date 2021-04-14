c7e065c45c69b2d4ac0bcf385ea1a72410ec7916
Emil Martens <emil.martens@gmail.com>
2021-04-14 17:37:16 +0200

    better plsr plot

--- src/plsr.py
+++ src/plsr.py
@@ -11,7 +11,7 @@ from adjustText import adjust_text
 import re
 # <--- This is important for 3d plotting
 from mpl_toolkits.mplot3d import Axes3D
-
+from matplotlib.lines import Line2D
 df, _ = get_hjortevillt_data()
 df = pre_process(df)
 df.drop(columns=df.columns[df.columns.str.contains('jaktfelt')], inplace=True)
@@ -32,11 +32,13 @@ diff = df.groupby('kommunenr').diff(1)
 X = df.loc[X_index].values
 Y = df.loc[Y_index, df.columns.str.contains(
     'sett')]['elg sett sum sette elg pr dag'].values[:, None]
+Y = df.loc[Y_index, df.columns.str.contains(
+    'sett')].values
+# Y = Y - X[:, :8]
+# Y = df.loc[Y_index].values - X
 plsr = PLSRegression(n_components=4, scale=True)
 plsr.fit(X, Y)
 
-# plt.plot(plsr.coef_[:, :])
-# plt.plot(plsr.coef_[:, :])
 
 x_scores = plsr.x_scores_
 y_scores = plsr.y_scores_
@@ -45,38 +47,56 @@ y_load = plsr.y_loadings_
 
 colors = X_index.get_level_values(0).values
 val, indices = np.unique(colors, return_inverse=True)
-# np.random.shuffle
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-# ax.scatter(x_scores[:, 0], x_scores[:, 1], x_scores[:, 2], c=indices,
-#            s=30, cmap='jet')
+
 loadings = plsr.x_loadings_
 L1 = loadings.T[0]
 L2 = loadings.T[1]
+x_load = plsr.x_loadings_.T[:2]
+y_load = plsr.y_loadings_.T[:2]
 
-
+plt.close('all')
 fig, ax = plt.subplots(1, 1)
+ax.set_xlim([-1, 1])
+ax.set_ylim([-1, 1])
 fig.set_size_inches(12, 8)
 plt.tight_layout()
+
 labels = [re.sub('elg ', '', label) for label in df.columns]
-colors = [0 if 'sett' in label else 1 for label in labels]
+colors = [0 if 'sett' in label else 0.5 for label in labels]
+colors += [1] * y_load.shape[1]
+labels += labels[:8]
 
 for remove in ['sett ', 'felt ', 'pr dag']:
     labels = [re.sub(remove, '', label) for label in labels]
 # labels = [re.sub('felt ', '', label) for label in labels]
+data = np.hstack([x_load, y_load])
 
-scatter = ax.scatter(L1, L2, c=colors)
-ax.legend(scatter.legend_elements()[0], ['Sett per jaktdøgn', 'Felt totalt'],
+colormap = plt.get_cmap()
+scatter = ax.scatter(*data, c=colors, cmap=colormap)
+# scatter_ny = ax.scatter(*plsr.y_loadings_.T[:2])
+# legend_elem = scatter_ny.legend_elements()[0] + scatter.legend_elements()[0]
+legend_elem = scatter.legend_elements()[0]
+
+
+legend_elements = [Line2D([0], [0], marker='o', color='w', label=name,
+                          markerfacecolor=color, markersize=10)
+                   for color, name in [
+                       [colormap(0), 'sett per jaktdøgn'],
+                       [colormap(0.5), 'felt'],
+                       [colormap(1.), 'utvikling'],
+
+]]
+
+ax.legend(handles=legend_elements,
           loc="lower left", title="")
-ax.scatter(*plsr.y_loadings_.ravel()[:2])
 
 
-ax.xaxis.zoom(-2)
-texts = [plt.text(L1[i], L2[i], labels[i], ha='left', va='center')
-         for i in range(len(df.columns))]
-adjust_text(texts, arrowprops=dict(arrowstyle='-', color='black'))
+texts = [plt.text(data[0, i], data[1, i], labels[i],
+                  ha='center', va='center')
+         for i in range(data.shape[1])]
 
-# for i, txt in enumerate(df.columns):
-#     ax.annotate(txt, (L1[i], L2[i]))
-# plt.show()
-# plsr.transform
+adjust_text(texts, arrowprops=dict(arrowstyle='-', color='black'),
+            force_points=(3, 0.3),
+            force_text=(0.1, 0.3),
+            # force_objects=(0.2, 0.4)
+            )
